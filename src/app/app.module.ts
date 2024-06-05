0f33e640a43bbaed9a63f6904bced7c1a823c37f
eltonhoxha91 <eltonhoxha2191@gmail.com>
2024-06-05 13:11:36 +0200

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule}     from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CribListingComponent } from './crib-listing/crib-listing.component';
+import { CribCardComponent } from './crib-card/crib-card.component';
+import { AddListingFormComponent } from './add-listing-form/add-listing-form.component';
+
+import {CribsService} from './services/cribs.service';
+import { UtilService } from'./services/util.service';
+import { SortByPipe } from './pipes/sort-by.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CribListingComponent,
+    CribCardComponent,
+    AddListingFormComponent,
+    SortByPipe,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+
+
   ],
-  providers: [],
+  providers: [CribsService,UtilService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule  { }
---
+++ src/app/crib-listing/crib-listing.component.ts
@@ -0,0 +1,47 @@
+import { Component,OnInit } from '@angular/core';
+import { HttpClient  }  from '@angular/common/http';
+import {CribsService} from   './../services/cribs.service';
+import {UtilService}  from   './../services/util.service';
+
+@Component({
+  selector: 'app-crib-listing',
+  templateUrl: './crib-listing.component.html',
+  styleUrls: ['./crib-listing.component.css']
+ })
+ export class CribListingComponent implements OnInit {
+
+ cribs: Array<any>;
+ error: string;
+ sortField: string= 'price';
+ sortDirection: string ='asc';
+ sortFields: Array<string>=  [
+ 'address',
+ 'area',
+ 'bathrooms',
+ 'bedrooms',
+ 'price',
+ 'type'
+ ];
+
+ constructor(
+  public  http: HttpClient,
+  private cribsService: CribsService,
+   public  utilService:  UtilService
+ ) { }
+
+  ngOnInit() {
+  this.cribsService.getAllCribs()
+  .subscribe(
+    data => this.cribs=data,
+    error => this.error = error.statusText
+    );
+  this.cribsService.newCribSubject.subscribe(
+  data=>this.cribs= [data,...this.cribs]
+  )
+}
+
+
+
+}
+
+
